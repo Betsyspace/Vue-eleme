7ff679989b80f9335e299eea292401e9cc501aa6
huanghuiqi <huiqihuang@126.com>
2017-03-01 15:23:08 +0800

    转化时间戳为标准日期js方法

---
+++ src/common/js/date.js
@@ -0,0 +1,21 @@
+/**
+ * Created by Administrator on 2017/3/1.
+ */
+export function formatDate(date,fmt){
+  if(/(y+)/.test(fmt)){
+    fmt = fmt.replace(RegExp.$1,(date.getFullYear()+'').substr(4-RegExp.$1.length));
+  }
+  let o ={
+    'M+':date.getMonth()+1,
+    'd+':date.getDate(),
+    'h+':date.getHours(),
+    'm+':date.getMinutes(),
+    's+':date.getSeconds()
+  }
+  for(let k in o){
+    if(new RegExp(`(${k}`).test(fmt)){
+      let str = o[k] + '';//转换为string
+      fmt = fmt.replace(RegExp.$1,)
+    }
+  }
+}
